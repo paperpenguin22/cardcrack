a6ca3ba872e70360650330ebf301b3eba67e32dc
EmoryL <131405598+EmoryL@users.noreply.github.com>
2025-06-03 14:48:40 +0000

    Accounts - Emory
    Started creating variables for accounts class

--- Accounts.java
+++ Accounts.java
@@ -1,6 +1,19 @@
 import java.io.*;
 import java.util.*;
+import java.nio.file.*;
+import java.time.LocalDate;
 
 public class Accounts{
-    static final String saveFile = "accounts";
+    private String email;
+    private String name;
+    private String password;
+    private Map <String, Map <String, List <String> > > subjects; //Nested Map - Subjects, Units, Questions
+    private List <String> friends;
+    private Map <LocalDate, TestInfo> calendar;
+    
+    static final String saveFile = "accounts.txt";
+
+    public static void save(Account account){
+        
+    }
 }
